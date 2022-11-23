c217a5c45582a7e6d67ff73ab5cb879f939735e8
gelterianaia <rama@gelaterianaia.com>
2022-11-23 21:36:49 +0530

    added shared prefrences for user info storage

--- lib/state/userinfo/notifier/userdetails.dart
+++ lib/state/userinfo/notifier/userdetails.dart
@@ -1,14 +1,37 @@
+import 'dart:convert';
+
 import 'package:dsd/state/auth/backend/authenticator.dart';
 import 'package:dsd/state/userinfo/backend/userdetailservice.dart';
 import 'package:dsd/state/userinfo/model/user.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserDetailsStateNotifier extends StateNotifier<User> {
   final _userSerevice = const UserDetailService();
   final _authenticator = const Authenticator();
+  //final prefs = await SharedPreferences.getInstance();
+  late SharedPreferences prefs;
+
+  bool validator(String? value) {
+    if (value!.isEmpty) {
+      return true;
+    }
+    return false;
+  }
+
+  Future _init() async {
+    prefs = await SharedPreferences.getInstance();
+    var userInfo = prefs.getString('userInfo');
+
+    if (validator(userInfo)) {
+      state = User.fromJson(json.decode(userInfo ?? ''));
+    }
+  }
 
   UserDetailsStateNotifier() : super(User.unknown()) {
     if (_authenticator.isAlreadyLoggedIn) {
+      _init();
+    } else {
       state = User(
         lastName: null,
         uuid: _authenticator.userId,
@@ -28,6 +51,8 @@ class UserDetailsStateNotifier extends StateNotifier<User> {
 
   Future<void> fetchUserDetails(String uuid) async {
     final result = await _userSerevice.fetchUserDetails(uuid);
+    final userinfo = result.toJson();
+    prefs.setString('userInfo', json.encode(userinfo));
     state = User(
       lastName: result.lastName,
       uuid: uuid,

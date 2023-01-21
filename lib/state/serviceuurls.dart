8836c515baa135110fa5b3ad8010f293ca0acb9b
gelterianaia <rama@gelaterianaia.com>
2023-01-21 14:36:44 +0530

    added customer url

---
+++ lib/state/serviceuurls.dart
@@ -0,0 +1,3 @@
+String baseURL ='https://us-central1-gelaterianaia-a3f12.cloudfunctions.net/app/';
+static customersURL ="${baseURL}customer/getcustomers";
+static createOrder ="${baseURL}order/createorder"
